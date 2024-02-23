4a4680cb9f4ed8be4f28f99b9f6fc289278ac025
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2024-02-23 14:07:53 -0300

    First commit

---
+++ easyadmin.js
@@ -0,0 +1,289 @@
+/**
+ * Easy Admin
+ *
+ * @copyright: Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license  : GNU/GPL http                         :                              //www.gnu.org/copyleft/gpl.html
+ */
+define(['jquery', 'fab/list-plugin'], function (jQuery, FbListPlugin) {
+	var FbListEasyadmin = new Class({
+		Extends: FbListPlugin,
+
+		initialize: function (options) { 
+            // Init options
+			this.options = options;
+			const { baseUri } = options;
+
+			const heading = jQuery('th.heading.fabrik_ordercell.fabrik_actions')[0];
+			const btnGroup = options.actionMethod == 'inline' ? jQuery(heading).find('.btn-group')[0] : jQuery(heading).find('.dropdown-menu')[0];
+			
+			if(btnGroup) {
+				// Adicionando o html do modal na página
+				const form = document.querySelector('.fabrikForm');
+
+				var modalContent = document.createElement('div');
+				modalContent.innerHTML = this.htmlModal();
+				form.appendChild(modalContent);
+
+				this.setButtons(options.elements, baseUri);
+				this.setActionPanel(options.elements);
+				jQuery(document).ready(function () {
+					jQuery(document).on('mouseenter', '.heading.fabrik_ordercell', function () {
+						jQuery(this).find(":button.elementAdminButton").show();
+					}).on('mouseleave', '.heading.fabrik_ordercell', function () {
+						jQuery(this).find(":button.elementAdminButton").hide();
+					});
+				});
+			} else {
+				return;
+			}
+			
+			// JQuery responsável por montar o modal na tela
+			jQuery("a[rel=modal]").click(function(ev){
+				ev.preventDefault();
+				var id = jQuery(this).attr("href");
+				var alturaTela  = jQuery(document).height();
+				var larguraTela = jQuery(window).width();
+		
+				jQuery('#mascara').css({'width':larguraTela, 'height':alturaTela});
+				jQuery('#mascara').fadeIn(200);
+				jQuery('#mascara').fadeTo("slow", 0.2);
+		
+				var left = (jQuery(window).width() / 2 ) - (jQuery(id).width() / 2 );
+				var top  = (jQuery(window).height() / 2 ) - (jQuery(id).height() / 2 );
+		
+				jQuery(id).css({'left':left, 'top':top});
+				jQuery(id).show();
+			});
+		
+			jQuery('#mascara').click(function(){		
+				jQuery(this).fadeOut("slow");
+				jQuery('.window').fadeOut("slow");
+			});
+		
+			jQuery('.fechar').click(function(ev){		
+				ev.preventDefault();		
+				jQuery('#mascara').fadeOut(200, "linear");
+				jQuery('.window').fadeOut(200, "linear");
+				window.location.reload();
+			});
+		},
+
+		// Create a button of an element edit link
+		// @link link of the button
+		createButton: function(link, id, baseUri) {
+			var button = jQuery('<a rel="modal" data-cooltipz-dir="right" href="#janela" class="tooltiptext2">' + this.options.images.edit +'</a>');
+			button.on('click', () => {
+				document.querySelector('#iframe-url').src = link;
+			});
+			return button;
+		},
+
+		// Set buttons to edit the elements
+		// @links array of the links
+		setButtons: function(links, baseUri)  {
+			for (var key in links) {
+				if(links.hasOwnProperty(key)) {
+					var element = jQuery('th.'+key);
+					element.addClass("tooltip2"); //Adiciona class do tooltip
+					var button  = this.createButton(links[key], key+"_admin_button", baseUri);
+					element.append(button);
+					element.css({
+						"min-width": "120px;"
+					});
+					
+				}
+			}
+		},
+
+		setActionPanel: function (links) {
+			if(this.options.actionMethod == 'inline') {
+				this.setActionPanelInline(links);
+			} else if (this.options.actionMethod == 'dropdown') {
+				this.setActionPanelDropdown(links);
+			} else {
+				throw new Error(Joomla.JText._("PLG_EASY_ADMIN_ACTION_METHOD_ERROR"));
+			}
+		},
+
+		setActionPanelInline: function (links) {
+			var self = this;
+			var button = jQuery('<a class="btn fabrik_view fabrik__rowlink btn-default"><span>' + this.options.images.admin + '</span><span class="hidden">Admin</span></a>');
+			var heading = jQuery('th.heading.fabrik_ordercell.fabrik_actions')[0];
+			var btnGroup = jQuery(heading).find('.btn-group')[0];
+			var JBtnGroup = jQuery(btnGroup);
+			var editListButton = jQuery('<li><button type="button">Editar Lista</button></li>');
+			
+			editListButton.on('click', () => {
+				window.open(self.options.listUrl, '_blank', menubar=false);
+			});
+			
+			editListButton.find('button').css({
+				'min-height': '30px',
+				'font-size': '12px',
+				'width': '100%',
+				'border-radius': '12px',
+				'color': '#fff',
+				'background-color': '#003EA1',
+			});
+
+			var div = jQuery("<div></div>");
+			div.css({
+				'font-size': '12px',
+				'position': 'absolute',
+				'z-index': 100,
+				'background-color' : "#FFF",
+				'display': 'none',
+				'right' : '50%',
+				'padding' : '10px',
+				'border': '2px solid #eee',
+				'border-radius': '4px',
+				'text-align': 'left',
+				'width': '150px',
+			});
+			
+			button.on('click', function () {
+				if(jQuery(div).css('display') == 'none') {
+					jQuery(div).css({'display': 'block' });
+				} else {
+					jQuery(div).css({'display': 'none' });
+				}
+				
+			});
+
+			JBtnGroup.append(button);
+
+			jQuery.each(links, function( index, value ) {
+				var li = jQuery('<li style="font-size: 12px"></li>')
+					.css({'font-size': '12px'})
+					.appendTo(div);
+				var aaa = jQuery('<a/>')
+					.text('- ' + self.options.elementsNames[index])
+					.css({
+						'cursor': 'pointer',
+						'padding-left': '10px',
+					})
+					.appendTo(li);
+				aaa.on('click', () => {
+					window.open(value, '_blank', menubar=false);
+				});
+			});
+
+			div.append(editListButton);
+			JBtnGroup.append(div);
+		},
+
+		setActionPanelDropdown: function (links) {
+			var self = this;
+
+			var button = jQuery('<li class="nav-link"><a title="Admin"><span>' + this.options.images.admin +'</span> Admin</a></li>');
+			var heading = jQuery('th.heading.fabrik_ordercell.fabrik_actions')[0];
+			var btnGroup = jQuery(heading).find('.dropdown-menu')[0];
+			var JBtnGroup = jQuery(btnGroup);
+			var editListButton = jQuery('<li class="subMenuAdmin" style="display: none; padding: 0px 10px;"><button type="button">Editar Lista</button></li>');
+			
+			editListButton.on('click', () => {
+				window.open(self.options.listUrl,'_blank', menubar=false);
+			});
+			
+			editListButton.find('button').css({
+				'min-height': '30px',
+				'font-size': '12px',
+				'width': '100%',
+				'border-radius': '12px',
+				'color': '#fff',
+				'background-color': '#003EA1',
+			});
+
+			button.on('click', function () {
+				jQuery.each(jQuery(this).parent().find('.subMenuAdmin'), function () {
+					if(jQuery(this).css('display') == 'none') {
+						jQuery(this).css({'display': 'block' });
+					} else {
+						jQuery(this).css({'display': 'none' });
+					}
+				});
+			});
+
+			jQuery.each(links, function(index, value) {
+				var li = jQuery('<li style="font-size: 12px; display: none;" class="subMenuAdmin"></li>')
+					.css({'font-size': '12px'})
+					.appendTo(button);
+				var aaa = jQuery('<a/>')
+					.text('- ' + self.options.elementsNames[index])
+					.css({
+						'cursor': 'pointer',
+						'padding-left': '10px',
+					})
+					.appendTo(li);
+				aaa.on('click', () => {
+					window.open(value,'_blank', menubar=false);
+				});
+			});
+
+			button.append(editListButton);
+			JBtnGroup.append(button);
+		},
+
+		htmlModal: function() {
+			return `
+			<style>
+				.window{	
+					display: none;
+					width: 90%;
+					height: 90vh;
+					position: absolute;
+					background: #FFF ;
+					left: 0;
+					top:0;
+					z-index: 9900;
+					border-radius: 10px;			
+				}
+				
+				#mascara{				
+					display: none;
+					position: absolute;
+					opacity: 0.2;
+					background: #000 ;
+					left: 0;
+					top:0;
+					z-index: 9000;			
+				}
+				
+				.fechar{				
+					display: block;
+					text-align: right;
+				}
+
+				.tooltip2 {
+					position: relative;
+					border-bottom: 1px dotted black;
+					cursor: pointer;
+				}
+
+				.tooltip2 .tooltiptext2 {
+					visibility: hidden;
+					background-color: #ccc;
+					color: #fff;
+					text-align: center;
+					padding: 5px;
+					border-radius: 6px;
+				}
+
+				.tooltip2:hover .tooltiptext2 {
+					visibility: visible;
+				}
+			</style>
+			<!-- <a href="#janela" rel="Modal">Abrir Janela Modal</a> -->
+			<div class="window" id="janela">
+				<a href="#" class="fechar" style="margin-right:10px; ">X Fechar</a>
+				<iframe id="iframe-url" height="100%" width="100%" src="#"></iframe>
+			</div>
+			<div id="mascara"></div>
+			`
+		}
+		
+		
+	});
+
+	return FbListEasyadmin;
+});
---
+++ easyadmin.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Fabrik List Javascript
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.list.js
+ * @copyright   Copyright (C) 2005-2020  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+// Require the abstract plugin class
+require_once COM_FABRIK_FRONTEND . '/models/plugin-list.php';
+
+/**
+ *  Add an action button to run PHP
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.list.js
+ * @since       3.0
+ */
+
+class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
+	private $images;
+	
+	function __construct(&$subject, $config) {
+		parent::__construct($subject, $config);
+	}
+
+	/*
+	* Init function
+	*/
+	protected function init() {
+		$this->jsScriptTranslation();
+		$opts = new StdClass;
+
+		$opts->baseUri = JURI::base();
+		$opts->elements = $this->processElements($this->model->getElements(true));
+		$opts->elementsNames = $this->processElementsNames($this->model->getElements(true));
+		$opts->listUrl = $this->createListLink($this->getModel()->getId());
+		$opts->actionMethod = $this->model->actionMethod();
+		$opts->images = $this->getImages();
+
+		// Load the JS code and pass the opts
+		$this->loadJS($opts);
+	}
+
+	/*
+	* Function to load the javascript code for the plugin
+	*/
+	protected function loadJS($opts) {
+		$optsJson = json_encode($opts);
+		$jsFiles = array();
+		$jsFiles['Fabrik'] = 'media/com_fabrik/js/fabrik.js';
+		$jsFiles['FabrikEasyAdmin'] = '/plugins/fabrik_list/easyadmin/easyadmin.js';
+		$script = "var fabrikEasyAdmin = new FabrikEasyAdmin($optsJson);";
+		FabrikHelperHTML::script($jsFiles, $script);
+	}
+
+	protected function processElements($elements) {
+		$processedElements = new stdClass;
+		foreach($elements as $key => $value) {
+			$fullElementName = $this->processFullElementName($key);
+			$processedElements->$fullElementName = $this->createLink($value->element->id);	
+		}
+		return $processedElements;
+	}
+
+	protected function processElementsNames($elements) {
+		$processedElements = new stdClass;
+		foreach($elements as $key => $value) {
+			$fullElementName = $this->processFullElementName($key);
+			$processedElements->$fullElementName = $value->element->label;	
+		}
+		return $processedElements;
+	}
+
+	protected function processFullElementName($key) {
+		// Select point on str
+		$pos = strpos($key, '.');
+		
+		// Get first part
+		$firstName = substr ($key , 1, $pos-2);
+		// Get second part
+		$lastName = substr ($key , $pos+2);
+		// Remove last char from secont part
+		$lastName = substr ($lastName , 0, strlen($lastName) - 1);
+		// Join them to the full name
+		$processedKey = $firstName . "___" . $lastName;
+		return $processedKey;
+	}
+
+	protected function createLink($elementId) {
+		$baseUri = JURI::base();
+		return $baseUri . "administrator/index.php?option=com_fabrik&view=element&layout=edit&id=". $elementId . "&modalView=1";
+	}
+
+	protected function createListLink($listId) {
+		$baseUri = JURI::base();
+		return $baseUri ."administrator/index.php?option=com_fabrik&view=list&layout=edit&id=". $listId . "&modalView=1";
+	}
+
+	/*
+	* Function run on when list is being loaded
+	* Used to trigger the init function
+	*/
+	public function onLoadData(&$args) {
+		$this->setImages();
+		$this->init();
+	}
+
+	public function setImages() {
+		$this->images['admin'] = FabrikHelperHTML::image('admin.png', 'list');
+		$this->images['edit'] = FabrikHelperHTML::image('edit.png', 'list');
+	}
+
+	public function getImages() {
+		return $this->images;
+	}
+
+	/**
+     * Function sends message texts to javascript file
+     *
+     * @since version
+     */
+    function jsScriptTranslation()
+    {
+        Text::script('PLG_EASY_ADMIN_ACTION_METHOD_ERROR');
+    }
+}
