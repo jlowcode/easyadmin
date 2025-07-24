4235fe7243271de070b0a05ea74c9feabf8f0e6c
David-Lucas17 <lucasehermilda@gmail.com>
2025-07-24 16:49:05 -0300

    feat: allows users registered on the list to approve requests

--- easyadmin.php
+++ easyadmin.php
@@ -241,7 +241,7 @@ class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
 	{
 		$app = Factory::getApplication();
 		$input = $app->input;
-		
+
 		if(
 			strpos($input->get('task'), 'filter') > 0 ||
 			strpos($input->get('task'), 'order') > 0 ||
@@ -505,7 +505,7 @@ class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
 	 * @param   	Object			$elements 		Object of each element of the list
 	 * @param   	Boolean			$mod 			Must be return label or name of the element
 	 * 
-	 * @return 		Object
+	 * @return 		Object		
 	 */
 	protected function processElementsNames($elements, $mod=true) 
 	{
@@ -1330,7 +1330,8 @@ class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
 		// Options to set up the element
 		$dEl->options = $this->optionsElements(Array(
 			'0' => Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_COLLAB_OPTION_0"),
-			'1' => Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_COLLAB_OPTION_1")
+			'1' => Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_COLLAB_OPTION_1"),
+			'2' => Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_COLLAB_OPTION_2"),
 		));
 		$dEl->name = $id;
 		$dEl->id = $id;
