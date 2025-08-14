9c908b0e5c839a26664364917fc1b399a12af237
David-Lucas17 <lucasehermilda@gmail.com>
2025-08-14 13:46:00 -0300

    refactor: aligns language constants and comments, adjusts default value of allow_delete

--- easyadmin.php
+++ easyadmin.php
@@ -1599,8 +1599,8 @@ class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
 
 		// Options to set up the element
 		$options = Array(
-			'0' => Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_LAYOUT_MODE_OPTION_0"),
-			'1' => Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_LAYOUT_MODE_OPTION_1")
+			'0' => Text::_("COM_FABRIK_LAYOUT_MODE_OPTION_0"),
+			'1' => Text::_("COM_FABRIK_LAYOUT_MODE_OPTION_1")
 		);
 
 		foreach ($elsList as $el) {
@@ -1611,12 +1611,12 @@ class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
 				$params->get('join_db_name') == $listModel->getTable()->get('db_table_name') && 
 				($params->get('database_join_display_style') == 'both-treeview-autocomplete' || $params->get('database_join_display_style') == 'only-treeview')
 			) {
-				$options['2'] = Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_LAYOUT_MODE_OPTION_2");
+				$options['2'] = Text::_("COM_FABRIK_LAYOUT_MODE_OPTION_2");
 			}
 		}
 		
 		if($listModel->canShowTutorialTemplate()) {
-			$options['3'] = Text::_("PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_LAYOUT_MODE_OPTION_3");
+			$options['3'] = Text::_("COM_FABRIK_LAYOUT_MODE_OPTION_3");
 		}
 
 		$dEl->options = $this->optionsElements($options);
@@ -1633,8 +1633,8 @@ class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
 		$elements[$id]['dataField'] = $dEl;
 		$elements[$id]['dataLabel'] = $this->getDataLabel(
 			$id,
-			Text::_('PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_LAYOUT_MODE_LABEL'),
-			Text::_('PLG_FABRIK_LIST_EASY_ADMIN_ELEMENT_LAYOUT_MODE_DESC'),
+			Text::_('COM_FABRIK_LAYOUT_MODE_LABEL'),
+			Text::_('COM_FABRIK_LAYOUT_MODE_DESC'),
 		);
 	}
 
@@ -4718,17 +4718,17 @@ class PlgFabrik_ListEasyAdmin extends PlgFabrik_List {
 				$dataList[$key]['workflow_list'] = $data['workflow_list'] == 'true' ? '1' : '0';
 
 				switch ((int)$data['collab_list']) {
-					case 2: //modo wiki
+					case 2: //Wiki mode
 						$dataList[$key]['allow_edit_details'] = '2';
 						$dataList[$key]['allow_add'] = '2';
 						break;
-					case 1: // modo open
-						$allowDelete = $dataList[$key]['allow_delete'] ?? '0';
+					case 1: //Open mode
+						$allowDelete = $dataList[$key]['allow_delete'] ?? '8';
 						$dataList[$key][$key]['allow_edit_details'] = $allowDelete;
 						$dataList[$key]['allow_add'] = '2';
 						break;
-					case 0: //modo restricted
-						$allowDelete = $dataList[$key]['allow_delete'] ?? '0';
+					case 0: //Restricted mode
+						$allowDelete = $dataList[$key]['allow_delete'] ?? '8';
 						$dataList[$key]['allow_edit_details'] = $allowDelete;
 						$dataList[$key]['allow_add'] = $allowDelete;
 						break;
